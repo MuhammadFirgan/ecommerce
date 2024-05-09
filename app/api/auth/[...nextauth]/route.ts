c558af6ac4fceacd85a507d5aa46cc5e0a4b0cd8
Antarcticpx <antarcticpx@gmail.com>
2024-05-09 12:36:12 +0700

    fix : fixed routes.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,11 +5,11 @@ import FacebookProvider from "next-auth/providers/facebook"
 export const authOption = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
         FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientId: process.env.FACEBOOK_CLIENT_ID!,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
         })
     ],
