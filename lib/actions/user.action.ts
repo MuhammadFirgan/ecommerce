ee11bac208619dfd59e2abfe7db61f3772c02cd7
Antarcticpx <antarcticpx@gmail.com>
2024-11-24 19:34:43 +0700

    feat(auth): fix auth system

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,20 @@
+'use server'
+import { createUserParams } from "@/types";
+import { dbConnect } from "../database";
+import User from "../database/models/user.model";
+
+
+
+export default async function createUser(user: createUserParams) {
+  try {
+    await dbConnect()
+
+    const newUser = await User.create(user)
+    console.log(newUser)
+
+    return JSON.parse(JSON.stringify(newUser))
+
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+export type createUserParams = {
+    clerkId: String
+    email: String
+    firstName: String | null
+    lastName: String | null
+    photo: String
+}
