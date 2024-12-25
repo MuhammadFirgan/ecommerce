c0301a377a13f09ef6c8c370921c6dee9a05299e
Antarcticpx <antarcticpx@gmail.com>
2024-12-25 13:37:27 +0700

    ..2

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { clerkClient } from '@clerk/express';
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
 import createUser from '@/lib/actions/user.action'
 import { NextResponse } from 'next/server';
 
@@ -66,7 +65,8 @@ export async function POST(req: Request) {
     const newUser = await createUser(user);
 
     if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      const client = await clerkClient()
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
         }
